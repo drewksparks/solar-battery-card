e2c3736f9dc8aa4e727c126b7096d4f4a7042145
Drew Sparks <drewksparks@gmail.com>
2026-03-03 15:05:11 -0500

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,9 +2,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: "b2500d-card.js",  
+  input: "battery-card.js",  
   output: {
-    file: "dist/b2500d-card.js",
+    file: "dist/battery-card.js",
     format: "es"
   },
   plugins: [
